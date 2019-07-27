81a4945ad39c77c3ed1f380ba051a9e6ef016482
Chris Opperwall <copperwall@gmail.com>
2019-07-27 09:49:03 -0700

    Add initial implementation
    This adds a dataloader implementation that tries to mimic the graphql
    reference implementation but with TypeScript and without classes.
    
    Not that classes are bad or whatever, it just seemed fun to try
    and write it another way.

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+type BatchFn<K, V> = (keys: ReadonlyArray<K>) => Promise<ReadonlyArray<V | Error>>;
+interface DataLoader<K, V> {
+    load: (key: K) => Promise<V>
+}
+interface QueueEntry<K> {
+    key: K,
+    resolve: Function,
+    reject: Function
+}
+
+function dataloader<K, V>(batchFn: BatchFn<K, V>): DataLoader<K, V> {
+    let queue: Array<QueueEntry<K>> = [];
+
+    function executeBatch(): void {
+        const oldQueue = queue;
+        queue = [];
+        const keys = oldQueue.map(entry => entry.key);
+
+        batchFn(keys).then(values => {
+            values.forEach(function (value, index) {
+                if (value instanceof Error) {
+                    oldQueue[index].reject(value);
+                } else {
+                    oldQueue[index].resolve(value);
+                }
+            });
+        });
+    }
+
+    return {
+        load: key => {
+            return new Promise((resolve, reject) => {
+                queue.push({
+                    key,
+                    resolve,
+                    reject
+                })
+
+                if (queue.length === 1) {
+                    process.nextTick(executeBatch);
+                }
+            });
+        }
+    }
+}
+
+export default dataloader;
